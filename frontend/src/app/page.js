8cfc8e97fa0c465d321ad51a6408b2536f804eed
22fi051 <22fi051@ms.dendai.ac.jp>
2024-10-10 01:10:40 +0900

    frontend

---
+++ frontend/src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">hello next</h1>
+    </div>
+  );
+}
